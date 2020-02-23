cf3192b924914720949eed12723bbd03d6fe6ff2
Dmytro <44460494+DmytroTitov@users.noreply.github.com>
2020-02-23 13:27:50 +0200

    HW-06 for review

--- js/task.js
+++ js/task.js
@@ -46,3 +46,49 @@ console.log(getUsersWithAge(users, 20, 30)); // [объект Ross Vazquez, об
 
 console.log(getUsersWithAge(users, 30, 40));
 // [объект Moore Hensley, объект Sharlene Bush, объект Blackburn Dotson, объект Sheree Anthony]
+
+// --------------task 7---------------------
+const calculateTotalBalance = users => {
+  return users.reduce((acc, user) => acc + user.balance, 0);
+};
+
+console.log(calculateTotalBalance(users)); // 20916
+
+// --------------task 8---------------------
+const getUsersWithFriend = (users, friendName) => {
+  return users
+    .filter(user => user.friends.some(friend => friend === friendName))
+    .map(user => user.name);
+};
+
+console.log(getUsersWithFriend(users, 'Briana Decker')); // [ 'Sharlene Bush', 'Sheree Anthony' ]
+console.log(getUsersWithFriend(users, 'Goldie Gentry')); // [ 'Elma Head', 'Sheree Anthony' ]
+
+// --------------task 9---------------------
+const getNamesSortedByFriendsCount = users => {
+  return [...users]
+    .sort(
+      (prevUser, nextUser) => prevUser.friends.length - nextUser.friends.length,
+    )
+    .map(user => user.name);
+};
+
+console.log(getNamesSortedByFriendsCount(users));
+// [ 'Moore Hensley', 'Sharlene Bush', 'Elma Head', 'Carey Barr', 'Blackburn Dotson', 'Sheree Anthony', 'Ross Vazquez' ]
+
+// --------------task 10---------------------
+const getSortedUniqueSkills = users => {
+  return users
+    .reduce((allUsersSkills, user) => {
+      user.skills.forEach(skill => {
+        if (!allUsersSkills.includes(skill)) {
+          allUsersSkills.push(skill);
+        }
+      });
+      return allUsersSkills;
+    }, [])
+    .sort();
+};
+
+console.log(getSortedUniqueSkills(users));
+// [ 'adipisicing', 'amet', 'anim', 'commodo', 'culpa', 'elit', 'ex', 'ipsum', 'irure', 'laborum', 'lorem', 'mollit', 'non', 'nostrud', 'nulla', 'proident', 'tempor', 'velit', 'veniam' ]
