689c2fc9641b70ace052bb75e1650b8a7abfe92a
Dmytro <44460494+DmytroTitov@users.noreply.github.com>
2020-02-24 19:36:39 +0200

    Reworked task-8 with includes

--- js/task.js
+++ js/task.js
@@ -57,7 +57,7 @@ console.log(calculateTotalBalance(users)); // 20916
 // --------------task 8---------------------
 const getUsersWithFriend = (users, friendName) => {
   return users
-    .filter(user => user.friends.some(friend => friend === friendName))
+    .filter(user => user.friends.includes(friendName))
     .map(user => user.name);
 };
 
